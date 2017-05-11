ad933c8e765615d9aed83dd321313597bddfaacf
Tom Hartman <thomas.lees.hartman@gmail.com>
2017-05-11 11:14:08 -0400

    Init file
    Added init file to be a bit more like a traditional package

---
+++ init.lua
@@ -0,0 +1,23 @@
+-------------------------------------------------------------------------------
+-- init.lua for awesome-pass                                                 --
+-- Copyright (c) 2017 Tom Hartman (thartman@hudco.com)                       --
+--                                                                           --
+-- This program is free software; you can redistribute it and/or             --
+-- modify it under the terms of the GNU General Public License               --
+-- as published by the Free Software Foundation; either version 2            --
+-- of the License, or the License, or (at your option) any later             --
+-- version.                                                                  --
+--                                                                           --
+-- This program is distributed in the hope that it will be useful,           --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of            --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             --
+-- GNU General Public License for more details.                              --
+-------------------------------------------------------------------------------
+
+--- Commentary -- {{{
+-- Init file for awesome-pass
+-- }}}
+
+--- init -- {{{
+return require('awesome-pass/awesome-pass')
+-- }}}
